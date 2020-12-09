1fda4c4168ec228b508b129f804b2c301529ee0c
Aiden Kyle Lingad <aklingad@gmail.com>
2020-12-09 17:15:04 +1300

    exception handling

--- Game of Life/Program.cs
+++ Game of Life/Program.cs
@@ -6,8 +6,47 @@ namespace Game_of_Life
     {
         static void Main(string[] args)
         {
-            Grid.InputGrid();
-            //Grid.Generate(5, 5, 0.5);
+            try
+            {
+                Run();
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("Sorry, an exception (developer-speak for ERROR) occured...");
+                Console.WriteLine(e);
+
+                //Console.Write("Would you like to report this to the developer? (y/n) ");
+                //if (Console.ReadLine() == "y")
+                //{
+                //    System.Diagnostics.Process.Start("explorer", "mailto:aklingad@gmail.com");
+                //}
+            }
+        }
+
+        private static void Run()
+        {
+            Console.Write("Would you like to: (1) generate a grid, or (2) manually input a grid? ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("length? ");
+                    int length = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("height? ");
+                    int height = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("life rate (from 0 to 1)? ");
+                    double liferate = Convert.ToDouble(Console.ReadLine());
+
+                    Grid.Generate(length, height, liferate);
+                    break;
+                case "2":
+                    Grid.InputGrid();
+                    break;
+                default:
+                    Console.WriteLine("You were not meant to do that! Do better next time.");
+                    Console.ReadKey();
+                    return;
+            }
 
             Grid.PrintGrid();
 
